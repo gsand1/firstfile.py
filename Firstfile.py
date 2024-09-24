3e4b9cae01a212e28c9449a4e3fca38a434dce44
gsand1 <gsand@stu.socsd.org>
2024-09-24 10:01:41 -0400

    Update Firstfile.py

--- Firstfile.py
+++ Firstfile.py
@@ -1,6 +1,8 @@
 #gabesand
 #9/23/24
-first= "gabe"
-last= "sand"
+first= "Gabe"
+last= "Sand"
 age= "14"
-print(first + last + age)
+
+print(first + ", " + last + ", " + age)
+print(first, last, age)
